14d7fa3b587008eebdd8e5b55a267eddaf6e3d59
lirlia <lilria5000@gmail.com>
2021-06-23 04:13:32 +0900

    add: DBの接続とcreate table

--- src/main.go
+++ src/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	logger "gorm.io/gorm/logger"
 )
 
-/*
 const (
-	DB_NAME                = "todo"
-	DB_USER                = "root"
-	DB_PASS                = "password"
-	DB_HOST                = "db"
-	DB_PORT                = "3306"
-	DB_ACCOUNT_TABLE_NAME  = "account"
-	DB_TODOLIST_TABLE_NAME = "todolist"
-	DB_INSERT_BATCHSIZE    = 3000
+	DB_NAME            = "todo"
+	DB_USER            = "root"
+	DB_PASS            = "password"
+	DB_HOST            = "localhost"
+	DB_PORT            = "3306"
+	DB_USER_TABLE_NAME = "user"
+	DB_TASK_TABLE_NAME = "task"
+
+	DB_INSERT_BATCHSIZE = 3000
 
 	// SetMaxIdleConnsはアイドル状態のコネクションプール内の最大数を設定します
 	DB_MAX_IDLE_CONN = 10
@@ -29,32 +31,20 @@ const (
 	DB_MAX_LIFETIME = time.Hour
 )
 
-type Account struct {
-	AccountID int `gorm:"primaryKey"`
-	Name      string
-	Password  string
-	CreatedAt time.Time
-}
-
-type Task struct {
-	TaskID    string `gorm:"primaryKey"`
-	Name      string
-	Done      bool
-	Message   string
-	AccountID string `gorm:"foreignKey"`
-	CreatedAt time.Time
-}
-
 // テーブル名を変換するために定義
-type Tabler interface {
+type Tables interface {
 	TableName() string
 }
 
 // テーブル名を決定する
-func (logMessage) TableName() string {
-	return DB_TABLE_NAME
+func (User) TableName() string {
+	return DB_USER_TABLE_NAME
+}
+func (Task) TableName() string {
+	return DB_TASK_TABLE_NAME
 }
 
+/*
 // MapからStructへの変換
 func MapToStruct(m map[string]string) logMessage {
 	reqtime, err := strconv.ParseFloat(m["reqtime"], 64)
@@ -72,41 +62,6 @@ func MapToStruct(m map[string]string) logMessage {
 	return logMessage{m["id"], t, m["level"], m["method"], m["uri"], reqtime, time.Now()}
 }
 
-// DBへの接続を行う関数
-func connectDB() *gorm.DB {
-	dsn := DB_USER + ":" + DB_PASS + "@tcp(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		// トランザクションの中で整合性を担保するための設定
-		// 今回は特定のレコードに対して複数の操作を行うことはないため性能向上を鑑みてtrueとする
-		// https://gorm.io/docs/performance.html#Disable-Default-Transaction
-		SkipDefaultTransaction: true,
-	})
-	if err != nil {
-		panic(err)
-	}
-	db.Logger = db.Logger.LogMode(logger.Silent)
-
-	sqlDB, err := db.DB()
-	// SetMaxIdleConnsはアイドル状態のコネクションプール内の最大数を設定します
-	sqlDB.SetMaxIdleConns(DB_MAX_IDLE_CONN)
-	// SetMaxOpenConnsは接続済みのデータベースコネクションの最大数を設定します
-	sqlDB.SetMaxOpenConns(DB_MAX_OPEN_CONN)
-	// SetConnMaxLifetimeは再利用され得る最長時間を設定します
-	sqlDB.SetConnMaxLifetime(DB_MAX_LIFETIME)
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
-
-// 構造体に沿ったテーブルの作成を行う
-func createTable(db *gorm.DB) {
-	// テーブルの存在をチェックしない場合のみ作る
-	if !db.Migrator().HasTable(&logMessage{}) {
-		// テーブルの作成
-		db.Migrator().CreateTable(&logMessage{})
-	}
-}
 
 // ログをDBにBatchINSERTする
 // DB_INSERT_BATCHSIZEで同時に格納するサイズを調整
@@ -189,9 +144,8 @@ func main() {
 	fmt.Println("finish")
 }
 */
-
-type Account struct {
-	AccountID int `gorm:"primaryKey"`
+type User struct {
+	UserID    int `gorm:"primaryKey"`
 	Name      string
 	Password  string
 	CreatedAt time.Time
@@ -202,53 +156,103 @@ type Task struct {
 	Name      string
 	Done      bool
 	Message   string
-	AccountID int `gorm:"foreignKey"`
+	UserID    int `gorm:"foreignKey"`
 	CreatedAt time.Time
 }
 
-func setTasktoList(task_id int, name string, msg string, done bool, account_id int) (r *Task) {
+func setTasktoList(task_id int, name string, msg string, done bool, user_id int) (r *Task) {
 	r = new(Task)
 	r.TaskID = task_id
 	r.Name = name
 	r.Message = msg
 	r.Done = done
-	r.AccountID = account_id
+	r.UserID = user_id
 	return r
 }
 
-type TaskList = []*Task
+//type TaskList = []*Task
 
-func setupRouter() *gin.Engine {
-	var tasks TaskList
+func setupRouter(db *gorm.DB) *gin.Engine {
 
-	tasks = append(tasks, setTasktoList(1, "これはテストです1", "ああああああ", false, 1))
-	tasks = append(tasks, setTasktoList(2, "これはテストです2", "ああああああ", true, 1))
-	tasks = append(tasks, setTasktoList(3, "これはテストです3", "ああああああ", true, 1))
+	//	var tasks TaskList
+	var task = Task{}
+
+	//	tasks = append(tasks, setTasktoList(1, "これはテストです1", "ああああああ", false, 1))
+	//	tasks = append(tasks, setTasktoList(2, "これはテストです2", "ああああああ", true, 1))
+	//	tasks = append(tasks, setTasktoList(3, "これはテストです3", "ああああああ", true, 1))
 
 	r := gin.Default()
 	r.Static("/static", "../static")
 	r.LoadHTMLGlob("../templates/*.*")
 
-	fmt.Println(tasks)
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 	r.POST("/mypage", func(c *gin.Context) {
-		var account = Account{
+		var user = User{
 			Name:     c.PostForm("username"),
 			Password: c.PostForm("password"),
 		}
 
 		c.HTML(http.StatusOK, "mypage.html", gin.H{
-			"username": account.Name,
-			"tasks":    tasks,
+			"username": user.Name,
+			"tasks":    db.Find(&task),
 		})
 	})
 
 	return r
 }
 
+// DBへの接続を行う関数
+func connectDB() *gorm.DB {
+	dsn := DB_USER + ":" + DB_PASS + "@tcp(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		// トランザクションの中で整合性を担保するための設定
+		// 今回は特定のレコードに対して複数の操作を行うことはないため性能向上を鑑みてtrueとする
+		// https://gorm.io/docs/performance.html#Disable-Default-Transaction
+		SkipDefaultTransaction: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	db.Logger = db.Logger.LogMode(logger.Silent)
+
+	sqlDB, err := db.DB()
+	// SetMaxIdleConnsはアイドル状態のコネクションプール内の最大数を設定します
+	sqlDB.SetMaxIdleConns(DB_MAX_IDLE_CONN)
+	// SetMaxOpenConnsは接続済みのデータベースコネクションの最大数を設定します
+	sqlDB.SetMaxOpenConns(DB_MAX_OPEN_CONN)
+	// SetConnMaxLifetimeは再利用され得る最長時間を設定します
+	sqlDB.SetConnMaxLifetime(DB_MAX_LIFETIME)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+// 構造体に沿ったテーブルの作成を行う
+func createTable(db *gorm.DB, t []Tables) {
+
+	for _, v := range t {
+		// テーブルの存在をチェックしない場合のみ作る
+		if !db.Migrator().HasTable(v) {
+			// テーブルの作成
+			db.Migrator().CreateTable(v)
+		}
+	}
+}
+
 func main() {
-	r := setupRouter()
+	// dbへの接続
+	db := connectDB()
+
+	// テーブルの作成を行う
+	type TableList = []Tables
+	var t TableList
+	t = append(t, &User{})
+	t = append(t, &Task{})
+	createTable(db, t)
+
+	r := setupRouter(db)
 	r.Run(":8080")
 }
